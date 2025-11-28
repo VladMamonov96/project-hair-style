882459ddeec11ecb30cb01289b8abbd30b756ca7
VladMamonov96 <vladik301096@gmail.com>
2025-11-29 02:59:08 +0300

    slider

--- src/main.js
+++ src/main.js
@@ -2,41 +2,27 @@ import 'swiper/swiper-bundle.css';
 import './assets/style/main.scss'
 
 
-// import Swiper from 'swiper';
-// import { Navigation, Pagination } from 'swiper/modules';
-// const swiper = new Swiper('.swiper', {
-//     modules: [Navigation, Pagination],
-//     direction: "vertical",
-//     speed: 700,
-//     mousewheel: true,
-// });
-//
-//
-// const items = document.querySelectorAll(".nav-item");
-// const nav = document.querySelector(".slider-nav");
-// const angles = [-55, -15, 25];
-//
-// function updateArcRotation() {
-//     items.forEach((item, i) => {
-//         const base = angles[i];
-//         const activeShift = swiper.activeIndex === i ? 8 : 0;
-//         item.style.transform = `rotate(${base + activeShift}deg)`;
-//     });
-// }
-//
-// updateArcRotation();
-//
-// swiper.on("slideChange", () => {
-//     items.forEach(i => i.classList.remove("active"));
-//     items[swiper.activeIndex].classList.add("active");
-//     updateArcRotation();
-//
-//     nav.classList.add("active-arc");
-//     setTimeout(() => nav.classList.remove("active-arc"), 300);
-// });
-//
-// /* колесо мыши по меню */
-// nav.addEventListener("wheel", e => {
-//     e.preventDefault();
-//     e.deltaY > 0 ? swiper.slideNext() : swiper.slidePrev();
-// });
+import Swiper from 'swiper';
+import { Pagination } from 'swiper/modules';
+
+
+let arrText = ['фен-стайлер 2 в 1','суперлёгкий','суперкомпактный','Инновационный\n японский мотор','ЭФФЕКТ САЛОННОГО\n ЛАМИНИРОВАНИЯ']
+
+const swiper = new Swiper('.swiper-vertical', {
+    modules: [Pagination],
+    speed: 700,
+    loop: true,
+    clickable: true,
+    direction: "vertical",
+    pagination: {
+        el: ".swiper-pagination",
+        dynamicBullets: true,
+        clickable: true,
+        renderBullet: function (index, className) {
+            return `<div class="${className} pagination">
+                      <div class="icon"></div>
+                      <p>${arrText[index + 1]}</p>
+                     </div>;`
+        }
+    },
+});
